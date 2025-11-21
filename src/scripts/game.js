80ec000908a1c89d516e2c39c1b573772f177c4a
syifa25 <umrimarsyifa2@gmail.com>
2025-11-21 23:52:06 +0700

    menambahkan fitur movement,collision, dan foreground

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -11,20 +11,6 @@ for (let i = 0; i < collisions.length; i+= 70){
     collisionsMap.push(collisions.slice(i, 70 + i));  
 }
 
-class Boundary {
-    static width = 48
-    static height = 48
-    constructor({position}){
-        this.position = position
-        this.width = 48
-        this.height = 48
-    }
-
-    draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
 
 const boundaries= []
 const offset = {
@@ -52,36 +38,13 @@ console.log(boundaries);
 const image = new Image()
 image.src = './src/img/untitled.png'
 
+const foregroundImage = new Image()
+foregroundImage.src = './src/img/foregroundObjek.png'
+
 const playerImage = new Image()
 playerImage.src = './src/img/playerDown.png'
 
-class Sprite{
-    constructor({ position, velocity, image, frames = {max:1}}){
-        this.position = position
-        this.image = image
-        this.frames = frames 
-        this.image.onload = () => 
-       {    this.width = this.image.width / this.frames.max
-            this.height = this.image.height
-            console.log(this.width)
-            console.log(this.height);
-       }
 
-    }
-    draw(){
-        c.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-        )
-    }
-}
     // canvas.width / 2 - this.image.width / 4 / 2,
     // canvas.height / 2 - this.image.height / 2,   
 const player = new Sprite({
@@ -103,6 +66,15 @@ const background = new Sprite({
     image: image 
 })
 
+const foreground = new Sprite({
+    position: {
+        x: offset.x, 
+        y: offset.y
+    },
+    image: foregroundImage
+})
+
+
 const keys = {
     w: {
         pressed : false
@@ -118,12 +90,7 @@ const keys = {
     }
 }
 
-const testBoundary = new Boundary({
-    position: {
-        x: 400,
-        y: 400
-    }
-})
+
 
 const movables = [background, ...boundaries]
 
@@ -141,51 +108,128 @@ function rectangularCollision({rectangle1, rectangle2}){
 function animate() {
   window.requestAnimationFrame(animate)
   background.draw()
-
   boundaries.forEach(boundary => {
     boundary.draw()
-    
   })
-    player.draw()
-  // Gambar player tetap di tengah
+  player.draw()
+  foreground.draw()
 
+  let moving = true 
+  player.moving = false
+
+  if (keys.w.pressed && lastKey === 'w') {
+    player.moving = true
+  
+    for(let i = 0; i < boundaries.length; i++){
+      const boundary = boundaries[i]
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y + 3
+            }  
+          }
+        })
+      ){
+        moving = false 
+        break
+      }
+    }
     
-
-    if (keys.w.pressed && lastKey === 'w') {
-        for(let i = 0; i < boundaries.length; i++){
-            const boundary = boundaries[i]
-            if (
-                rectangularCollision({
-                    rectangle1: player,
-                    rectangle2 : {...boundary,
-                        position:{
-                            x:boundary.position.x,
-                            y:boundary.position.y + 3
-                        }  
-                    }
-                })
-            ){
-        console.log('colliding')
-        }
-        }
-        movables.forEach((movable) => {
-            movable.position.y += 3})
+    
+    if (moving) {
+      movables.forEach((movable) => {
+        movable.position.y += 3
+      })
     }
-    else if (keys.a.pressed && lastKey === 'a') {
-        movables.forEach((movable) => {
-            movable.position.x += 3
+  }
+  else if (keys.a.pressed && lastKey === 'a') {
+    player.moving = true
+    
+    for(let i = 0; i < boundaries.length; i++){
+      const boundary = boundaries[i]
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x + 3,
+              y: boundary.position.y
+            }  
+          }
         })
+      ){
+        moving = false
+        break
+      }
     }
-    else if (keys.s.pressed && lastKey === 's') {
-        movables.forEach((movable) => {
+    
+    if (moving) {
+      movables.forEach((movable) => {
+        movable.position.x += 3
+      })
+    }
+  }
+  else if (keys.s.pressed && lastKey === 's') {
+    player.moving = true
+    // Cek collision untuk arah bawah
+    for(let i = 0; i < boundaries.length; i++){
+      const boundary = boundaries[i]
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y - 3
+            }  
+          }
+        })
+      ){
+        moving = false
+        break
+      }
+    }
+    
+    if (moving) {
+      movables.forEach((movable) => {
         movable.position.y -= 3
-        })
+      })
     }
-    else if (keys.d.pressed && lastKey === 'd') {
-        movables.forEach((movable) => {
+  }
+  else if (keys.d.pressed && lastKey === 'd') {
+    player.moving = true
+    // Cek collision untuk arah kanan
+    for(let i = 0; i < boundaries.length; i++){
+      const boundary = boundaries[i]
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x - 3,
+              y: boundary.position.y
+            }  
+          }
+        })
+      ){
+        moving = false
+        break
+      }
+    }
+    
+    if (moving) {
+      movables.forEach((movable) => {
         movable.position.x -= 3
-        })
+      })
     }
+  }
 }
 
 animate()
