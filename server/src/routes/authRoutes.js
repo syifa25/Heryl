bce683384bdcdfe7bee84605f1d4ad411cc7fd62
syifa25 <umrimarsyifa2@gmail.com>
2025-12-06 21:05:19 +0700

    Fix lock file

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { register, login } = require("./controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
